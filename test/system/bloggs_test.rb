ee220239c367ea204a1203f892a903cdfa1f01b7
aditi <aditybhayana@gmail.com>
2019-10-24 10:26:39 -0400

    initia commit

---
+++ test/system/bloggs_test.rb
@@ -0,0 +1,9 @@
+require "application_system_test_case"
+
+class BloggsTest < ApplicationSystemTestCase
+  # test "visiting the index" do
+  #   visit bloggs_url
+  #
+  #   assert_selector "h1", text: "Blogg"
+  # end
+end
